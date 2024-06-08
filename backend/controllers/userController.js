55f43b0cec377f27318fc907d519eb5e27f8afcd
Shunt <shunt_ps@hotmail.com>
2024-06-08 15:01:58 -0400

    Add logout functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -121,7 +121,20 @@ const loginUser = asyncHandler(async (req, res) => {
    }
 });
 
+// Logout user
+const logout = asyncHandler(async (req, res) => {
+   res.cookie("token", "", {
+      path: "/",
+      httpOnly: true,
+      expires: new Date(0),
+      sameSite: "none",
+      secure: true,
+   });
+   return res.status(200).json({ message: "Succesfully Logged Out" });
+});
+
 module.exports = {
    registerUser,
    loginUser,
+   logout,
 };
