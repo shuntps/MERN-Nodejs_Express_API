c590273581b9edf8ba4c639a0893d91b9341f3ae
Shunt <shunt_ps@hotmail.com>
2024-06-09 15:44:27 -0400

    Add update user function

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -6,12 +6,19 @@ const {
    loginUser,
    logout,
    getUser,
+   loginStatus,
+   updateUser,
 } = require("../controllers/userController");
 const protect = require("../middleware/authMiddleware");
 
+// Unprotected routes
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/logout", logout);
+router.get("/loggedin", loginStatus);
+
+// Protected routes
 router.get("/getUser", protect, getUser);
+router.patch("/updateuser", protect, updateUser);
 
 module.exports = router;
